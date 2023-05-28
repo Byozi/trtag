cc5abc2e3faa91d276a8e65aed6b13de4963e976
Halil K <64667314+Byozi@users.noreply.github.com>
2023-05-28 13:34:02 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ import os
 class Config():
     # Get these values from my.telegram.org
     # https://my.telegram.org
-    API_ID = int(os.environ.get("API_ID"))
+    API_ID = int(os.environ.get("26880818"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     BOT_USERNAME = os.environ.get("BOT_USERNAME")

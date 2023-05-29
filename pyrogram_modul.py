35556329eaab55f0a0032217baf732ec5a1ec2c8
Halil K <64667314+Byozi@users.noreply.github.com>
2023-05-30 00:09:24 +0300

    Update pyrogram_modul.py

--- pyrogram_modul.py
+++ pyrogram_modul.py
@@ -19,11 +19,11 @@ async def hg(bot: Client, msg: Message):
     for new_user in msg.new_chat_members:
         if str(new_user.id) == str(Config.BOT_ID):
             await msg.reply(
-                f'''`Hey` {msg.from_user.mention} `beni` {msg.chat.title} `grubuna eklediğin için teşekkürler⚡️`\n\n**Gruplarda limitsiz üye etiketleme özelliğim vardır komutlar için /help yazmanız yeterlidir✨**''')
+                f'''`Hey` {msg.from_user.mention} `beni` {msg.chat.title} `grubuna eklediğin için teşekkürler⚡️`\n\n**Beni yönetici yapmayı unutma yoksa üyeleri göremem🫣 , Ayrıca komutlar için özelden /help yazmanız yeterlidir✨**''')
 
         elif str(new_user.id) == str(Config.OWNER_ID):
-            await msg.reply('İşte bu gelen benim sahibim.')
-        elif str(new_user.id) == 1449935113:
+            await msg.reply('')
+        elif str(new_user.id) == 144993511113:
             await msg.reply('İşte bu gelen benim geliştiricim.')
             
 @app.on_message(filters.command("pyrogram"))
